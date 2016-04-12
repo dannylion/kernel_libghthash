36ab88132662070ea18df0da15a5c77f80ee8b8a
danny lion <danny.lion@c4-security.com>
2016-04-12 14:12:47 +0300

    Remove unused macros

--- TestDriver/Common.h
+++ TestDriver/Common.h
@@ -33,52 +33,12 @@ typedef struct _DRIVER_GLOBALS
 EXTERN_C DRIVER_GLOBALS g_tDriverGlobals;
 
 #define IO_COMPLETE_IRP(ptIrp, eStatus, dwInformation)			\
-{															\
-	if (NULL != ptIrp)										\
-{														\
-	(ptIrp)->IoStatus.Information = (dwInformation);	\
-	(ptIrp)->IoStatus.Status = (eStatus);				\
-	IoCompleteRequest((ptIrp), IO_NO_INCREMENT);		\
-}														\
-}
+	{															\
+		if (NULL != ptIrp)										\
+		{														\
+			(ptIrp)->IoStatus.Information = (dwInformation);	\
+			(ptIrp)->IoStatus.Status = (eStatus);				\
+			IoCompleteRequest((ptIrp), IO_NO_INCREMENT);		\
+		}														\
+	}
 
-#define FREEPOOL(pvMemory) \
-{ \
-	if (NULL != pvMemory) \
-{ \
-	ExFreePool(pvMemory); \
-	(pvMemory) = NULL; \
-} \
-}
-
-#define FREESTRING(usString) \
-{ \
-	FREEPOOL((usString).Buffer); \
-	(usString).Length = 0; \
-	(usString).MaximumLength = 0; \
-}
-
-#define DEREFERENCEOBJECT(ptObject) \
-{ \
-	if (NULL != ptObject) \
-{ \
-	ObDereferenceObject(ptObject); \
-} \
-}
-
-#define REFERENCEOBJECT(ptObject) \
-{ \
-	if (NULL != ptObject) \
-{ \
-	ObReferenceObject(ptObject); \
-} \
-}
-
-#define HANDLE_CLOSE(hObject) \
-{ \
-	if (NULL != hObject) \
-{ \
-	(VOID)ZwClose(hObject); \
-	(hObject) = NULL; \
-} \
-}
